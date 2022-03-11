261aedbb25ee399f8f2e378d184ea14406a01b63
rohit <rohit063kr@gmail.com>
2022-03-11 13:52:00 +0530

    feature-card

--- src/components/Feature/FeatureCard.js
+++ src/components/Feature/FeatureCard.js
@@ -19,7 +19,14 @@ const FeatureCard = function (props) {
       <a href="#" className="btn btn-sm btn--round mb-li">
         {props.linkTitle}
       </a>
-      <span className="feature-card__time-update mb-sm">{props.time}</span>
+      <p className="feature-card__text feature-card__time-update mb-sm">
+        turies, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      {/* <span className="feature-card__time-update mb-sm">{props.time}</span> */}
     </div>
   );
 };
